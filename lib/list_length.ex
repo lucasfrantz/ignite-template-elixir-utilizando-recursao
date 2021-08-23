e9e25b3f1dbb088c45133b2c7505c83ab5cb86a3
Lucas Frantz <maxkronus@gmail.com>
2021-08-23 20:41:25 -0300

    Implementação da Função de Count

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,9 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: count(list, 0)
+
+  def count([_head | tail], count) do
+    count(tail, count + 1)
   end
+
+  def count([], count), do: count
 end
